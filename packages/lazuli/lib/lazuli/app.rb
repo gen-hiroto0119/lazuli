6b5a9a6c1e8a93fa486d97666e12963898b9ef48
Hiroto / hiroto furugen <hiro_genfuru0119@icloud.com>
2025-12-16 09:02:38 +0000

    feat(core): improve routing and renderer responses

--- packages/lazuli/lib/lazuli/app.rb
+++ packages/lazuli/lib/lazuli/app.rb
@@ -2,7 +2,10 @@ require "rack"
 
 module Lazuli
   class App
-    def initialize
+    def initialize(root: nil, socket: nil)
+      @app_root = File.expand_path(root || ENV["LAZULI_APP_ROOT"] || Dir.pwd)
+      ENV["LAZULI_APP_ROOT"] ||= @app_root
+      Lazuli::Renderer.configure(socket_path: socket)
       start_deno_process
     end
 
@@ -15,24 +18,32 @@ module Lazuli
 
       # Asset Proxy
       if path.start_with?("assets/")
-        content = Lazuli::Renderer.asset("/" + path)
-        # TODO: Set correct content type based on extension
-        return [200, { "content-type" => "application/javascript" }, [content]]
+        response = Lazuli::Renderer.asset("/" + path)
+        status = response[:status] || 500
+        headers = normalize_headers(response[:headers])
+        headers["content-type"] ||= content_type_for(path)
+        body = response[:body].to_s
+        return [status, headers, [body]]
       end
 
       # Simple routing: /users -> UsersResource#index
       path = "home" if path.empty?
 
-      resource_name = "#{path.capitalize}Resource"
+      segments = path.split("/")
+      resource_name = "#{segments.first.capitalize}Resource"
+      action = resolve_action(req.request_method, segments)
       
       begin
         # Try to find the resource class
         # In a real app, we would autoload these
         resource_class = Object.const_get(resource_name)
         resource = resource_class.new(req.params)
-        
-        # Assume 'index' action for now
-        html = resource.index
+
+        unless resource.respond_to?(action)
+          return [404, { "content-type" => "text/plain" }, ["Action not found: #{resource_name}##{action}"]]
+        end
+
+        html = resource.public_send(action)
         
         [200, { "content-type" => "text/html" }, [html]]
       rescue NameError
@@ -44,10 +55,51 @@ module Lazuli
 
     private
 
+    def resolve_action(method, segments)
+      id_present = segments.length > 1 && !segments[1].to_s.empty?
+      case method
+      when "GET"
+        id_present ? "show" : "index"
+      when "POST"
+        "create"
+      when "PUT", "PATCH"
+        "update"
+      when "DELETE"
+        "destroy"
+      else
+        "index"
+      end
+    end
+
+    def normalize_headers(headers)
+      (headers || {}).transform_keys do |key|
+        key.to_s.downcase
+      end
+    end
+
+    def content_type_for(path)
+      case File.extname(path)
+      when ".js", ".mjs", ".ts", ".tsx"
+        "application/javascript"
+      when ".css"
+        "text/css"
+      when ".json"
+        "application/json"
+      when ".svg"
+        "image/svg+xml"
+      when ".png"
+        "image/png"
+      when ".jpg", ".jpeg"
+        "image/jpeg"
+      else
+        "application/octet-stream"
+      end
+    end
+
     def start_deno_process
       # TODO: Implement process management
       # For now, we assume the user started Deno manually or we print instructions
-      puts "[Lazuli] Ensure Deno renderer is running on #{Lazuli::Renderer::SOCKET_PATH}"
+      puts "[Lazuli] Ensure Deno renderer is running on #{Lazuli::Renderer.socket_path}"
     end
   end
 end
--- packages/lazuli/lib/lazuli/renderer.rb
+++ packages/lazuli/lib/lazuli/renderer.rb
@@ -4,14 +4,29 @@ require "net/http"
 
 module Lazuli
   class Renderer
-    SOCKET_PATH = "tmp/sockets/lazuli-renderer.sock"
+    DEFAULT_SOCKET_PATH = File.expand_path(
+      ENV["LAZULI_SOCKET"] ||
+      File.join(ENV["LAZULI_APP_ROOT"] || Dir.pwd, "tmp", "sockets", "lazuli-renderer.sock")
+    )
 
     def self.render(page, props)
-      new.post("/render", { page: page, props: props })
+      response = new.post("/render", { page: page, props: props })
+      if response[:status] && response[:status] >= 400
+        raise Lazuli::Error, "Render failed (#{response[:status]}): #{response[:body]}"
+      end
+      response[:body]
     end
 
     def self.asset(path)
       new.get(path)
+    end
+
+    def self.configure(socket_path: nil)
+      @socket_path = socket_path if socket_path
+    end
+
+    def self.socket_path
+      @socket_path || DEFAULT_SOCKET_PATH
     end
 
     def post(path, data)
@@ -26,7 +41,7 @@ module Lazuli
 
     def request(method, path, body = nil)
       begin
-        sock = UNIXSocket.new(SOCKET_PATH)
+        sock = UNIXSocket.new(self.class.socket_path)
         
         req = "#{method} #{path} HTTP/1.1\r\n" \
               "Host: localhost\r\n" \
@@ -46,12 +61,29 @@ module Lazuli
         response = sock.read
         sock.close
         
-        # Extract body (very naive parsing)
-        # In a real app, we should parse headers to get Content-Type, etc.
-        _headers, body = response.split("\r\n\r\n", 2)
-        body
+        headers_raw, body = response.split("\r\n\r\n", 2)
+        status = 500
+        headers = {}
+
+        if headers_raw
+          header_lines = headers_raw.split("\r\n")
+          status_line = header_lines.shift
+          if status_line && status_line =~ %r{HTTP/\d\.\d\s+(\d+)}
+            status = Regexp.last_match(1).to_i
+          end
+          header_lines.each do |line|
+            key, value = line.split(": ", 2)
+            headers[key.downcase] = value if key && value
+          end
+        end
+
+        { status: status, headers: headers, body: body }
       rescue Errno::ENOENT, Errno::ECONNREFUSED
-        "Lazuli Renderer Error: Could not connect to Deno renderer at #{SOCKET_PATH}"
+        {
+          status: 502,
+          headers: {},
+          body: "Lazuli Renderer Error: Could not connect to Deno renderer at #{self.class.socket_path}"
+        }
       end
     end
   end
