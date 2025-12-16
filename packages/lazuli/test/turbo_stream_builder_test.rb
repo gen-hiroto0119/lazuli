ea07656342c5807d205b28d0135f87c48e573aeb
Hiroto / hiroto furugen <hiro_genfuru0119@icloud.com>
2025-12-16 16:00:07 +0000

    test: cover turbo stream target/targets

---
+++ packages/lazuli/test/turbo_stream_builder_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+require "lazuli/turbo_stream"
+
+class TurboStreamBuilderTest < Minitest::Test
+  def test_target_ops_emit_target_only
+    t = Lazuli::TurboStream.new
+
+    t.append "a", fragment: "components/Row", props: {}
+    t.prepend "b", fragment: "components/Row", props: {}
+    t.before "c", fragment: "components/Row", props: {}
+    t.after "d", fragment: "components/Row", props: {}
+    t.update "e", fragment: "components/Row", props: {}
+    t.replace "f", fragment: "components/Row", props: {}
+    t.remove "g"
+
+    ops = t.operations
+
+    ops[0..5].each do |op|
+      assert op.key?(:target)
+      refute op.key?(:targets)
+    end
+
+    assert_equal({ action: :remove, target: "g" }, ops[6])
+  end
+
+  def test_targets_ops_emit_targets_only
+    t = Lazuli::TurboStream.new
+
+    t.append fragment: "components/Row", props: {}, targets: ".row"
+    t.prepend fragment: "components/Row", props: {}, targets: ".row"
+    t.before fragment: "components/Row", props: {}, targets: ".row"
+    t.after fragment: "components/Row", props: {}, targets: ".row"
+    t.update fragment: "components/Row", props: {}, targets: ".row"
+    t.replace fragment: "components/Row", props: {}, targets: ".row"
+    t.remove targets: ".row"
+
+    ops = t.operations
+
+    ops[0..5].each do |op|
+      assert op.key?(:targets)
+      refute op.key?(:target)
+    end
+
+    assert_equal({ action: :remove, targets: ".row" }, ops[6])
+  end
+
+  def test_invalid_fragment_is_rejected
+    t = Lazuli::TurboStream.new
+    assert_raises(ArgumentError) { t.append "a", fragment: "../secrets", props: {} }
+  end
+end
